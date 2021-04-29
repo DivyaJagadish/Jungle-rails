d64a1623f045d73727a7ef10e58b3e193f81c18d
Divya Jagadish <divyajagadish21@gmail.com>
2021-04-29 19:48:49 +0000

    Display counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
 http_basic_authenticate_with name: ENV['Admin_Login'], password: ENV['Admin_password']
   def show
+    @countproduct = Product.count
+    @countcategory = Category.count
+
   end
 end
