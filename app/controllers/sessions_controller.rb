e7db3d2d0312fb7dc8d8a76f9dd2a3bdcfac43a8
Divya Jagadish <divyajagadish21@gmail.com>
2021-05-03 23:40:27 +0000

    validates email uniqueness

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  def new 
+  end 
   def create
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
