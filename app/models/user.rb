02f750bacbc8e86013509f05817a39a23075eaae
Divya Jagadish <divyajagadish21@gmail.com>
2021-05-04 03:34:21 +0000

    Adds validations to email, lastname,firstname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
    has_secure_password
-    validates :email, uniqueness: true
+   validates :email, uniqueness: true, case_senstive: false,presence: true
+   validates :firstname, presence: true
+   validates :lastname, presence: true
+
 end
